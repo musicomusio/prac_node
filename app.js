14ed4d5ff9790aa4933158c72010de4ff79015c7
musico <musicomusio@gmai.com>
2014-08-18 01:20:03 +0900

    join css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/',function (req,res) {
 	res.render('index',{title: "EXPRESS Sample"});
